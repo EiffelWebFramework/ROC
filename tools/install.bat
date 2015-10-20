3f4e70b98c58e5a6ee1d6719e41bf306c881fde6
Jocelyn Fiat <git@djoce.net>
2015-10-20 11:29:16 +0200

    Updated roc tools, and associated scripts. Also include Eiffel Store ODBC persistence.

--- tools/install.bat
+++ tools/install.bat
@@ -4,6 +4,7 @@ set TMP_EXCLUDE=%~dp0.install_ROC-copydir-exclude
 set COPYCMD= xcopy /EXCLUDE:%TMP_EXCLUDE% /I /E /Y 
 set TMP_DIR=%~dp0..
 set SAFE_RMDIR=rd /q/s
+set SAFE_RM=del
 
 echo EIFGENs > %TMP_EXCLUDE%
 echo .git >> %TMP_EXCLUDE%
@@ -60,6 +61,10 @@ echo Install ROC as CMS ewf
 %COPYCMD% %TMP_DIR%\doc	%TMP_UNSTABLE_DIR%\library\web\cms\doc
 %COPYCMD% %TMP_DIR%\modules	%TMP_UNSTABLE_DIR%\library\web\cms\modules
 %COPYCMD% %TMP_DIR%\examples	%TMP_UNSTABLE_DIR%\library\web\cms\examples
+
+%COPYCMD% %TMP_DIR%\tools	%TMP_UNSTABLE_DIR%\library\web\cms\tools
+%SAFE_RM% %TMP_UNSTABLE_DIR%\library\web\cms\tools\install.bat
+%SAFE_RM% %TMP_UNSTABLE_DIR%\library\web\cms\tools\safe_md.bat
 
 copy %TMP_DIR%\cms.ecf %TMP_UNSTABLE_DIR%\library\web\cms\cms.ecf
 copy %TMP_DIR%\cms-safe.ecf %TMP_UNSTABLE_DIR%\library\web\cms\cms-safe.ecf
